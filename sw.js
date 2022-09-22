7c39f76459620e6206854315d9a462957dfdcbcb
zhouhuafei <1123486116@qq.com>
2022-09-22 21:56:02 +0800

    fix: 🐛 world

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ self.addEventListener('install', async (event) => { // 主要用以缓存内容
   // 缓存内容
   const cache = await caches.open(cacheName)
   await cache.addAll([
-    '/www/github-zhouhuafei/hello-wrold_service-worker/',
-    '/www/github-zhouhuafei/hello-wrold_service-worker/index.css'
+    '/www/github-zhouhuafei/hello-world_service-worker/',
+    '/www/github-zhouhuafei/hello-world_service-worker/index.css'
   ])
 
   await self.skipWaiting()
